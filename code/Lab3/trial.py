f83b4dd260f96b5e98e5a3d3676f8635e34eccca
Jeremy <jtkawahigashi@gmail.com>
2024-12-03 15:26:05 -0800

    first commit

---
+++ code/Lab3/trial.py
@@ -0,0 +1,29 @@
+import tensorflow as tensorflow
+from tensorflow import keras
+from keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+
+layers = tensorflow.keras.layers
+model = keras.Sequential([
+    layers.Dense(512, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="rmsprop",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
+
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_digits = test_images[0:10]
+predictions = model.predict(test_digits)
+predictions[0]
+
+print("hello")
